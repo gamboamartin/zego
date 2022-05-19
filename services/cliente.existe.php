b7333a3fd90ea33778372dc033b89a4920e9d947
mgamboa <gamboa.martin@gmail.com>
2022-05-19 10:27:18 -0500

    v1

---
+++ services/cliente.existe.php
@@ -0,0 +1,67 @@
+<?php
+include "init.php";
+const PATH_BASE = '/var/www/html/zego/';
+
+
+require PATH_BASE.'vendor/autoload.php';
+
+use gamboamartin\errores\errores;
+use gamboamartin\services\services;
+use gamboamartin\calculo\calculo;
+use config\empresas;
+use models\cliente;
+
+$services = new services(path: __FILE__);
+$calculo = new calculo();
+
+$empresas = new empresas();
+$empresas_data = $empresas->empresas;
+
+foreach ($empresas_data as $empresa){
+
+
+    $conexiones = $services->conexiones(empresa: $empresa);
+    if(errores::$error){
+        $error = (new errores())->error('Error al conectar', $conexiones);
+        print_r($error);
+        die('Error');
+    }
+
+    var_dump($conexiones);
+
+    $cliente_modelo_remota = new cliente(link: $conexiones->remote);
+    $cliente_modelo_local = new cliente(link: $conexiones->local);
+
+    $where = 'cliente.existe = 0';
+    $r_clientes = $cliente_modelo_remota->registros_puros(limit: 100, tabla: 'cliente', where: $where);
+    if(errores::$error){
+        $error = (new errores())->error('Error al obtener registros', $r_clientes);
+        print_r($error);
+        die('Error');
+    }
+    $clientes = $r_clientes['registros'];
+    foreach($clientes as $cliente){
+        $cliente_id = $cliente['id'];
+        $existe_en_local = $cliente_modelo_local->existe_por_id($cliente_id, 'cliente');
+        if(errores::$error){
+            $error = (new errores())->error('Error al obtener cliente local', $existe_en_local);
+            print_r($error);
+            die('Error');
+        }
+
+        if($existe_en_local){
+            $cliente_remoto_upd['existe'] = 1;
+            $cliente_remoto_upd['status'] = 1;
+            $upd = $cliente_modelo_remota->modifica_bd($cliente_remoto_upd, 'cliente', $cliente_id);
+            if(errores::$error){
+                $error = (new errores())->error('Error al actualizar cliente remoto', $upd);
+                print_r($error);
+                die('Error');
+            }
+            var_dump($upd);
+        }
+    }
+
+
+}
+$services->finaliza_servicio();
